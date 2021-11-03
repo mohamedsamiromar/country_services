8a44f2ebca990fa0a47de95544c755f233da78ae
mohamed samir <56839321+mohamedsamiromar@users.noreply.github.com>
2021-11-04 00:08:00 +0200

    change the MY_APP_URL in seting

--- autoui/settings.py
+++ autoui/settings.py
@@ -23,10 +23,10 @@ SECRET_KEY = 'django-insecure-8ngs)3t=ydrnzvy9&e!2rpu^l!5d#t$6@_8r-1g9_2(t+s3k39
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+# http://67.207.72.87/accounts/facebook/login/
+#  Site.objects.create(domain='http://67.207.72.87/accounts/facebook/login/', name='http://67.207.72.87/accounts/facebook/login/')
 ALLOWED_HOSTS = ['67.207.72.87', 'autourapi.cf', '127.0.0.1', 'localhost']
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -56,7 +56,7 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/google/callback'
 LOGOUT_REDIRECT_URL = '/'
-MY_APP_URL = 'http://67.207.72.87'
+MY_APP_URL = 'http://autourapi.cf'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
@@ -178,3 +178,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'samir155mohamed@gmail.com'
 EMAIL_HOST_PASSWORD = 'hifjrhwyjovywgcc'
+
+
