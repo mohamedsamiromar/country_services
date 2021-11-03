ff4a4bfe5fde4ff24cf63643db06772a8c724dfa
mohamed samir <56839321+mohamedsamiromar@users.noreply.github.com>
2021-11-03 19:30:13 +0200

    add the new ALLOWED_HOSTS

--- autoui/settings.py
+++ autoui/settings.py
@@ -24,8 +24,7 @@ SECRET_KEY = 'django-insecure-8ngs)3t=ydrnzvy9&e!2rpu^l!5d#t$6@_8r-1g9_2(t+s3k39
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['67.207.72.87', 'autourapi.cf']
-
+ALLOWED_HOSTS = ['67.207.72.87', 'autourapi.cf', '127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
