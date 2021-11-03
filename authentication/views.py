4fcd28d6d1542dea20958c64c08eb461807b191b
mohamed samir <56839321+mohamedsamiromar@users.noreply.github.com>
2021-11-03 20:20:29 +0200

    add the new ALLOWED_HOSTS

--- authentication/views.py
+++ authentication/views.py
@@ -36,12 +36,11 @@ class RegisterView(generics.CreateAPIView):
             user.email = email
             user.username = username
             user.set_password(password)
-            token = Token.objects.get_or_create(user=request.user)
             user.save()
-            return Response(token, status=status.HTTP_201_CREATED)
+            return Response(RegisterSerializer(user).data, status=status.HTTP_201_CREATED)
         return Response(RegisterSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
-# python -m venv authentication autoui
-# source authentication autoui/bin/activate
+
+
 class VerifyEmail(APIView):
     permission_classes = (AllowAny,)
 
