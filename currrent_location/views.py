8fe0c86547c028b6ced344b1b949d8bde7d71902
mohamed samir <56839321+mohamedsamiromar@users.noreply.github.com>
2021-11-23 00:07:14 +0200

    Add documentation for every feature in current_location App

--- currrent_location/views.py
+++ currrent_location/views.py
@@ -10,6 +10,13 @@ from currrent_location.serializers import CurrentLocationSerializer, CurrentLoca
     SelecteCountryAndCitySerializer
 from .pagination import LargeResultsSetPagination, StandardResultsSetPagination
 
+'''
+ get_current_location 
+ If the user will allow the GPS location
+ that will get user city, country, region_name, longitude and latitude
+ and then save that in the table CurrentLocation()
+'''
+
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
@@ -39,11 +46,22 @@ def get_current_location(request):
     return Response(CurrentLocationSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+'''
+if user doesn't allowed the GPS,here we will display countries and cities and the user can selet his own country 
+and city, countries with cities, the countries and cities created by admin model
+'''
+
+
 class GetCurrentLocationaManualy(generics.ListAPIView):
     permission_classes = [AllowAny]
     queryset = GetLocationManual.objects.all()
     serializer_class = CurrentLocationManualSerializer
     pagination_class = LargeResultsSetPagination
+
+
+'''
+here will send id GetLocationManual in url the user chose, and save that
+'''
 
 
 @api_view(['GET'])
