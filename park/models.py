3090691e7aec29abd8948335a99679586cb29626
mohamed samir <56839321+mohamedsamiromar@users.noreply.github.com>
2022-05-24 19:34:56 +0200

    Feat(parking) Parking model

--- park/models.py
+++ park/models.py
@@ -1,3 +1,31 @@
+from hashlib import blake2s
+from http import server
+from re import S, T
+import re
+from statistics import mode
+from tkinter.tix import Tree
+from xmlrpc.client import TRANSPORT_ERROR
 from django.db import models
-
+from accounts.models import BaseModel, CustomUser
 # Create your models here.
+
+
+class ParkPofile(BaseModel):
+    country = [
+        ('FRA', 'French'),
+        ('SWZ', 'Switzerland'),
+        ('BLG', 'Belgium'),
+    ]
+    user = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING, related_name='user_park', blank=True)
+    name = models.CharField(max_length=150, null=True, blank=True)
+    mobile_number = models.IntegerField(max_length=11, null=True, blank=True, )
+    country = models.CharField(max_length=55, choices=country, null=True, blank=True, default=False)
+    city = models.CharField(max_length=55, null=True, blank=True)
+    status = models.CharField(max_length=155, choices=(
+            (201, 'Open'),
+            (405, 'Closed'),
+        ), default=201, null=True, blank=True)
+    address = models.TextField(max_length=350, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
